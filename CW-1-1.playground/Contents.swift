deef87c8d0ec1ca1d103c2cca603d42722f71edb
AzizAlhaider <azizalhaider03@gmail.com>
2020-09-09 20:17:41 +0300

    Cw Done

---
+++ CW-1-1.playground/Contents.swift
@@ -0,0 +1,19 @@
+let name = "Abdulaziz"
+var hobby1 = "football"
+var hobby2 = "swimming"
+var age = 17
+var height = 1.76
+
+print("my name is \(name) my age is: \(age)")
+
+let friendName = "YOUSEF"
+var friendHeight = 1.8
+var friendAge = 20
+var friendHobby1 = "football"
+var friendHobby2 = "cooking"
+
+var age_difference = friendAge - age
+ print(age_difference)
+
+var samehobby = hobby1 == friendHobby1 || hobby1 == friendHobby2 || hobby2 == friendHobby1 || hobby2 == friendHobby2
+ print(samehobby)
